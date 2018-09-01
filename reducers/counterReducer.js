6fe6adaeabda3a320ada005032cf8c57127cf39a
guram sanikidze <sanikidze.guram@gmail.com>
2018-09-01 19:07:12 +0400

    updated reducer

--- reducers/counterReducer.js
+++ reducers/counterReducer.js
@@ -4,13 +4,9 @@ import { initialStore } from 'STORE'
 export default (state = initialStore, action) => {
     switch (action.type) {
         case actionTypes.INCREMENT:
-            return Object.assign({}, state, {
-                count: state.count + 1
-            })
+            return { ...state, count: state.count + 1 }
         case actionTypes.DECREMENT:
-            return Object.assign({}, state, {
-                count: state.count - 1
-            })
+            return { ...state, count: state.count - 1 }
         default: return state
     }
 }
